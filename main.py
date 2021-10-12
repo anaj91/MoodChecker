054cdfb961479db515625cd91e8dafd6760e33fc
Ana Jenko <ana.jenko91@gmail.com>
2021-10-12 20:31:35 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#Lekcija 6 (1. domača  naloga)
+
+mood = input("How are you feeling today?")
+
+if mood == "happy":
+    print("It\'s good to see you happy.")
+elif mood == "sad":
+    print("Cheer up, mate!")
+elif mood == "nervous":
+    print("Take a deep breath and relax.")
+elif mood == "excited":
+    print("Look how far you\' ve come.")
+elif mood == "relaxed":
+    print("You probably just finished meditating")
+else:
+    print("I don\'t know the feeling")
